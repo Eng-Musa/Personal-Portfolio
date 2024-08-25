69fd25c82015d7b65fd7c38745b01d8797d59ee4
Musa <mangabomoses@gmail.com>
2024-08-25 11:35:14 +0300

    Image responsiveness on smaller screens done, padding added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SummaryComponent } from './components/summary/summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'summary', pathMatch:'full'},
+    {path:'summary', component:SummaryComponent}
+];
