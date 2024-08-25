6bc7e1aea77fbe12d0fdce0aaef0ade7b62822f1
Musa <mangabomoses@gmail.com>
2024-08-25 15:49:00 +0300

    Routing to dashbord error fix, resume component addition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SummaryComponent } from './components/summary/summary.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'summary', pathMatch:'full'},
-    {path:'summary', component:SummaryComponent}
+    {path:'summary', component:SummaryComponent},
+    {path:'resume', component:ResumeComponent}
 ];
