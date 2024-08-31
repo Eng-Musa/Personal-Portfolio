b55603d0da81ca44744d5d9cb075495d5203bf69
Musa <mangabomoses@gmail.com>
2024-08-31 17:35:58 +0300

    Animate projects only when in viewport

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements AfterViewInit{
+
+  private hasLoggedError = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      if ('IntersectionObserver' in window) {
+        const observerOptions = {
+          root: null, // Use the viewport as the root
+          rootMargin: '0px',
+          threshold: 0.1 // Trigger when 10% of the element is visible
+        };
+
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              const target = entry.target as HTMLElement;
+
+              // Add the appropriate animation class based on the element's class
+              if (target.classList.contains('content-left')) {
+                target.classList.add('animate-left');
+              } else if (target.classList.contains('content-right')) {
+                target.classList.add('animate-right');
+              } else if (target.classList.contains('content-up')) {
+                target.classList.add('animate-up');
+              } 
+            }
+          });
+        }, observerOptions);
+
+        // Observe the elements
+        document.querySelectorAll('.content-left, .content-right, content-up').forEach(element => {
+          observer.observe(element);
+        });
+      } else {
+        console.error('Intersection Observer API is not supported in this browser.');
+      }
+    } else {
+      if (!this.hasLoggedError) {
+        console.error('This code is not running in a browser environment.');
+        this.hasLoggedError = true;
+      }
+    }
+  }
 
 }
