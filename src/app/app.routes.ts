9bb42d3ec9bc60173a8cc8d02114aefdd6378475
Musa <mangabomoses@gmail.com>
2024-08-31 18:00:03 +0300

    Contact routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SummaryComponent } from './components/summary/summary.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'summary', pathMatch:'full'},
     {path:'summary', component:SummaryComponent},
     {path:'resume', component:ResumeComponent},
-    {path:'projects', component:ProjectsComponent}
+    {path:'projects', component:ProjectsComponent},
+    {path:'contact', component:ContactComponent}
 ];
